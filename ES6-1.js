8a1665267a0edf61938f80535c074ab265b19ccf
krups88 <b88pl88@gmail.com>
2023-01-23 19:42:55 +0100

    es-1

--- ES6-1.js
+++ ES6-1.js
@@ -16,3 +16,9 @@ const PI = freezeObj();
 const magic = () => {
     return new Date();
 };
+
+
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+console.log(myConcat([1, 2], [3, 4, 5]));
