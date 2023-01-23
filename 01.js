ec233646a20a1fcacbceb4e807272e73e399a4ee
krups88 <b88pl88@gmail.com>
2023-01-23 15:39:45 +0100

    start

--- 01.js
+++ 01.js
@@ -1,1 +1,12 @@
 /*bohdan*//*git HTMLButtonElement*/ 
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x === y) {
+result = "Equal!";
+} else {
+result = "Not equal!";
+}
+
+console.log(result);
