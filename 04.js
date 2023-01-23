46a2d215819a937ea916eeb96102e8088ea063ae
krups88 <b88pl88@gmail.com>
2023-01-23 16:52:32 +0100

    04

---
+++ 04.js
@@ -0,0 +1,10 @@
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Змініть код лише під цим рядком
+    for (let i = 0; i <= len-1; i++) {
+    // Змініть код лише над цим рядком
+        console.log(firstFive[i]);
+    }
+}
+countToFive();
