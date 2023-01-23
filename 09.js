ab1f8dc283a39785ef2b69680d18e8023064ccb2
krups88 <b88pl88@gmail.com>
2023-01-23 18:07:34 +0100

    09

---
+++ 09.js
@@ -0,0 +1,8 @@
+function mixedNumbers(arr) {
+    // Змініть код лише під цим рядком
+arr.unshift('I', 2, 'three');
+arr.push(7, 'VIII', 9)
+    // Змініть код лише над цим рядком
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
